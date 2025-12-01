705fd448b983f1f964ab18ee55255270053cbf28
Gabriel-Rodrigues01 <163764950+Gabriel-Rodrigues01@users.noreply.github.com>
2025-12-01 00:51:49 -0300

    Criação da pasta dao(Data Access Object)

--- src/main/java/org/dao/LivroDAO.java
+++ src/main/java/org/dao/LivroDAO.java
@@ -1,2 +1,36 @@
-package org.dao;public class LivroDAO {
+package org.dao;
+
+import jakarta.persistence.EntityManager;
+import org.model.Livro;
+import java.util.List;
+
+public class LivroDAO {
+    private EntityManager em;
+
+    public LivroDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void salvar(Livro livro) {
+        em.getTransaction().begin();
+        if (livro.getId() == null) {
+            em.persist(livro);
+        } else {
+            em.merge(livro);
+        }
+        em.getTransaction().commit();
+    }
+
+    public void remover(Long id) {
+        em.getTransaction().begin();
+        Livro livro = em.find(Livro.class, id);
+        if (livro != null) {
+            em.remove(livro);
+        }
+        em.getTransaction().commit();
+    }
+
+    public List<Livro> listarTodos() {
+        return em.createQuery("SELECT l FROM Livro l", Livro.class).getResultList();
+    }
 }
