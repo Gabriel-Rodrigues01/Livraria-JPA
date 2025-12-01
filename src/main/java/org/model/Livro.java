e7c09539128d02f1019e4106eade3274f13dd250
Gabriel-Rodrigues01 <163764950+Gabriel-Rodrigues01@users.noreply.github.com>
2025-12-01 00:50:24 -0300

    Criação da pasta model

--- src/main/java/org/model/Livro.java
+++ src/main/java/org/model/Livro.java
@@ -1,2 +1,27 @@
-package org.model;public class Livro {
+package org.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String autor;
+    private Double preco;
+
+    public Livro() {}
+    public Livro(String titulo, String autor, Double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+    // Getters e Setters OBRIGATÓRIOS para o JavaFX TableView ler os dados
+    public Long getId() { return id; }
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public String getAutor() { return autor; }
+    public void setAutor(String autor) { this.autor = autor; }
+    public Double getPreco() { return preco; }
+    public void setPreco(Double preco) { this.preco = preco; }
 }
